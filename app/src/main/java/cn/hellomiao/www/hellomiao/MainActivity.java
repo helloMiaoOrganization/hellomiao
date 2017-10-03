641829592ea6c60bdda287e2f4d90ee9655e5cc8
test1072 <1072844432@qq.com>
2017-10-03 11:35:26 +0800

    这是另一个账号发的

--- app/src/main/java/cn/hellomiao/www/hellomiao/MainActivity.java
+++ app/src/main/java/cn/hellomiao/www/hellomiao/MainActivity.java
@@ -3,6 +3,7 @@ package cn.hellomiao.www.hellomiao;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//这是首页
 public class MainActivity extends AppCompatActivity {
 
     @Override
